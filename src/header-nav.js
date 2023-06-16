e0b23995915ca3c65cb1111e24e7fa9ae16ca7ea
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-06-16 11:53:08 +0200

    generacion del header y nav a partir de una funcion

---
+++ src/header-nav.js
@@ -0,0 +1,18 @@
+import Logo from './logo.svg';
+
+export default function generateHeaderNav() {
+  let mainTitle = document.querySelector('#title');
+  let logoElement = document.querySelector('#logo');
+  let navHome = document.querySelector('#nav-home');
+  let navMenu = document.querySelector('#nav-menu');
+  let navAbout = document.querySelector('#nav-about');
+
+  //Header elements
+  mainTitle.textContent = 'Restaurant';
+  logoElement.src = Logo;
+
+  //Menu nav elements
+  navHome.textContent = 'Home';
+  navMenu.textContent = 'Menu';
+  navAbout.textContent = 'About';
+}
