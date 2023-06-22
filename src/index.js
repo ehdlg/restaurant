db938f1b360271bb58f5e018e2208e004841d242
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-06-22 11:35:58 +0200

    importacion de funciones de generacion y borrado de elementos dom, objeto con las diferentes funciones de generacion de elementos y addEventListener para la barra de menu que generan las diferentes paginas a traves de modulos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import generateHome from './home';
-import './style.css';
+import generateMenu from './menu';
+import generateAbout from './about';
+import generateHeaderNav from './header-nav';
+import './styles/layout.css';
+import { clearContent, isMenuSelected } from './functions/functions.js';
+
+const contentFunctions = {
+  home: generateHome,
+  menu: generateMenu,
+  about: generateAbout,
+};
+
+//Variable que recoge los elemento <li> del menu de navegación
+let navElements = document.querySelectorAll('.list-element');
+
+navElements.forEach((element) => {
+  element.addEventListener('click', function () {
+    if (!isMenuSelected(this)) {
+      let sectionToGenerate =
+        contentFunctions[this.dataset.section] ?? generateHome;
+      clearContent();
+      sectionToGenerate();
+    }
+  });
+});
+
+//Se genera el header y nav de la pagina con la funcion del modulo 'generateHeaderNav.js'
+generateHeaderNav();
 generateHome();
