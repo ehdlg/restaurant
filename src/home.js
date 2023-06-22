8b026b6a33ad2d5dc6a9c8bc2ff8785ee4433812
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-06-22 11:41:20 +0200

    importados los estilos de la seccion, creacion de un array de objetos con la informacion del restaurante y la funcion exportada que genera el contenido de la seccion inicial

--- src/home.js
+++ src/home.js
@@ -1,7 +1,69 @@
-import RestaurantIcon from './restaurant.jpg';
+import RestaurantIcon from './img/restaurant.jpg';
+import './styles/home.css';
+//Array de objetos con informacion del restaurante
+const cardsInfo = [
+  {
+    title: 'About Us',
+    text: 'Located in the heart of the city, "Delicious Bites" is a culinary haven for food enthusiasts. Our restaurant is dedicated to providing exceptional service and creating memorable moments for our guests.',
+    img: RestaurantIcon,
+  },
+  {
+    title: 'Ambiance',
+    text: 'Step into our elegant and inviting space, where contemporary design meets warm hospitality. Our stylish decor and comfortable seating provide the perfect backdrop for an unforgettable dining experience.',
+    img: RestaurantIcon,
+  },
+  {
+    title: 'Cuisine',
+    text: 'Indulge in a culinary journey through our diverse menu, inspired by international flavors. From savory appetizers to mouthwatering main courses and delightful desserts, our carefully curated selection showcases the artistry and passion of our culinary team.',
+    img: RestaurantIcon,
+  },
+  {
+    title: 'Private Events',
+    text: "Whether you're celebrating a special occasion or hosting a corporate gathering, our restaurant offers a private event space that can accommodate your needs. We provide personalized menus and impeccable service to ensure a memorable event for you and your guests.",
+    img: RestaurantIcon,
+  },
+  {
+    title: 'Reservations',
+    text: 'To guarantee a table at "Delicious Bites," we recommend making a reservation in advance. Our friendly staff will be delighted to assist you in securing your preferred date and time.',
+    img: RestaurantIcon,
+  },
+];
 export default function generateHome() {
   let content = document.querySelector('#content');
-  let home = document.createElement('div');
-  let title = document.createElement('h1');
-  let restaurantIcon = document.createElement('img');
+  let subtitle = document.createElement('h2');
+  subtitle.textContent = 'Home';
+  subtitle.id = 'subtitle';
+  content.appendChild(subtitle);
+
+  cardsInfo.forEach((info) => {
+    content.appendChild(generateHomeCard(info));
+  });
+}
+
+/**
+ * Funcion que genera una card con el objeto info pasado por parametro
+ */
+function generateHomeCard(info) {
+  let card = document.createElement('div');
+  card.classList.add('card-home');
+
+  let cardBody = document.createElement('div');
+  cardBody.classList.add('card-home-body');
+
+  let titleElement = document.createElement('h3');
+  titleElement.textContent = info.title;
+  card.appendChild(titleElement);
+
+  let imgElement = document.createElement('img');
+  imgElement.src = info.img;
+  imgElement.alt = `${info.title} image.`;
+  cardBody.appendChild(imgElement);
+
+  let textElement = document.createElement('p');
+  textElement.textContent = info.text;
+  cardBody.appendChild(textElement);
+
+  card.appendChild(cardBody);
+
+  return card;
 }
